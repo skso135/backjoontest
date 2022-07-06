75116075f2e112e2ff93de70a9e00163942b9596
kch <67400782+skso135@users.noreply.github.com>
2022-07-06 16:00:51 +0900

    Add files via upload

---
+++ backjoon.py
@@ -0,0 +1,17 @@
+#1000
+# a, b =map(int, input().split())
+# print(a+b)
+
+#1001
+# a, b =map(int, input().split())
+# print(a-b)
+
+#1008
+# a, b =map(int, input().split())
+# print(a/b)
+
+#1271 엄청난 부자2
+#돈n, 생명체m (1<=m<=n<=10000 / m,n은 10진수 정수)
+n,m = map(int, input().split())
+print(n//m)
+print(n%m)
